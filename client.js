9fa048562be5937e661d5da41eaff63a1f554937
HamzaYaqoobi <45055194+HamzaYaqoobi@users.noreply.github.com>
2018-11-14 20:05:50 -0700

    Add files via upload

---
+++ client.js
@@ -0,0 +1,64 @@
+$(function() {
+    Let pet_info = {
+    name: "Hamza's Pup",
+    weight: 5,
+    happiness: 7
+    }
+  
+  // Called function to update the name, happiness, and weight of our pet in our HTML
+  checkAndUpdatePetInfoInHtml();
+
+  // When each button is clicked, it will "call" function for that button (functions are below)
+  $('.treat-button').click(clickedTreatButton);
+  $('.play-button').click(clickedPlayButton);
+  $('.exercise-button').click(clickedExerciseButton);
+  
+
+
+  // Add a variable "pet_info" equal to a object with the name (string), weight (number), and happiness (number) of your pet
+
+  function clickedTreatButton() {
+    // Increase pet happiness
+    pet_info.happiness++;
+    // Increase pet weight
+    pet_info.weight++;
+    checkAndUpdatePetInfoInHtml();
+  }
+  
+  function clickedPlayButton() {
+    // Increase pet happiness
+    pet_info.happiness++;
+    // Decrease pet weight
+    pet_info.weight--;
+    checkAndUpdatePetInfoInHtml();
+  }
+  
+  function clickedExerciseButton() {
+    // Decrease pet happiness
+    pet_info.happiness--;
+    // Decrease pet weight
+    pet_info.weight--;
+    checkAndUpdatePetInfoInHtml();
+  }
+
+  function checkAndUpdatePetInfoInHtml() {
+    checkWeightAndHappinessBeforeUpdating();  
+    updatePetInfoInHtml();
+  }
+  
+  function checkWeightAndHappinessBeforeUpdating() {
+    // Add conditional so if weight is lower than zero, set it back to zero
+    if(pet_info.weight === 0) {
+      //pet_ino.weight = 0;
+      alert ("FEED YOUR DOG");
+  }
+  if(pet_info.happiness < 0) {
+    pet_info.happiness t=
+    
+  // Updates your HTML with the current values in your pet_info object
+  function updatePetInfoInHtml() {
+    $('.name').text(pet_info.name);
+    $('.weight').text(pet_info.weight);
+    $('.happiness').text(pet_info.happiness);
+  }
+})
